324657b72d92783aabde6dd02133f7928ae2d288
Kanguli05 <willykanguli@gmail.com>
2024-11-08 10:50:48 +0300

    Order Item card update status

--- src/components/OrderItemCard.tsx
+++ src/components/OrderItemCard.tsx
@@ -1,16 +1,33 @@
-import { Order } from "@/types";
+import { Order, OrderStatus } from "@/types";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Separator } from "./ui/separator";
 import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 import { ORDER_STATUS } from "@/config/order-status-config";
+import { useUpdateMyRestaurantOrder } from "@/api/MyRestaurantApi";
+import { useEffect, useState } from "react";
 
 type Props = {
     order: Order;
 }
 
 const OrderItemCard = ({order}: Props) => {
+
+    const { updateRestaurantStatus, isLoading} =  useUpdateMyRestaurantOrder();
+
+    const [status, setStatus] = useState <OrderStatus>(order.status);
+
+    useEffect(() => {
+        setStatus(order.status);
+    }, [order.status]);
+
+    const handleStatusChange =  (newStatus: OrderStatus) => {
+        updateRestaurantStatus({orderId: order._id as string, status: newStatus})
+        setStatus(newStatus);
+    };
+
+    
 
     const getTime = () => {
         const orderDateTime = new Date(order.createdAt);
@@ -64,7 +81,7 @@ const OrderItemCard = ({order}: Props) => {
                 </div>
                 <div className="flex flex-col space-y-1.5">
                     <Label htmlFor="status">What is the status of this order</Label>
-                    <Select>
+                    <Select disabled={isLoading} value={status} onValueChange={(value) => handleStatusChange(value as OrderStatus)}>
                         <SelectTrigger>
                             <SelectValue placeholder="Status" />
                         </SelectTrigger>
