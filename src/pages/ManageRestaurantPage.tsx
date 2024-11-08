93913e8ea6552372b806c7242c74eacb1cbefe37
Kanguli05 <willykanguli@gmail.com>
2024-11-08 09:54:17 +0300

    Restaurant order status page ORDER_STATUS

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,5 +1,7 @@
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
-import { useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
+import { useCreateMyRestaurant, useGetMyRestaurant, useGetMyRestaurantOrders, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import OrderItemCard from "@/components/OrderItemCard";
 
 export default function ManageRestaurantPage() {
 
@@ -7,12 +9,34 @@ export default function ManageRestaurantPage() {
   const { restaurant } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: isUpdateLoading} = useUpdateMyRestaurant();
 
+  const { orders }= useGetMyRestaurantOrders();
+
   const isEditing = !!restaurant;
 
   return (
-    <ManageRestaurantForm
-     restaurant={restaurant}
-     onSave={isEditing ? updateRestaurant : createRestaurant} 
-     isLoading={isCreateLoading || isUpdateLoading}/>
-  )
+
+    <Tabs defaultValue="orders">
+      <TabsList>
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value="orders" className="space-y-5 bg-gray-50 pg-10 rounded-lg">
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => <OrderItemCard  order= {order}/>)}
+      </TabsContent>
+
+      <TabsContent value="manage-restaurant" className="space-y-5 bg-gray-50 pg-10 rounded-lg">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing? updateRestaurant : createRestaurant} 
+          isLoading={isCreateLoading || isUpdateLoading}/>
+      </TabsContent>
+
+
+
+    </Tabs>
+
+   
+  );
 }
