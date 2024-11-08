cb312505d4fe9c208a8344efe206ecbc639dde00
Kanguli05 <willykanguli@gmail.com>
2024-11-08 15:03:31 +0300

    my user api changes on createMyuser request class

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -48,20 +48,28 @@ export const useCreateMyUser = () => {
     const {getAccessTokenSilently} = useAuth0();
 
     const createMyUserRequest = async( user: CreateUserRequest) => {
-        const accessToken = await getAccessTokenSilently();
-        const response = await fetch(`${API_BASE_URL}/api/my/user`, {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(user),
-        });
+        try{
+            const accessToken = await getAccessTokenSilently();
+            const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(user),
+            });
 
-        if (!response.ok) {
+            if (!response.ok) {
+                throw new Error("Failed to create user");
+            }
+
+            return response.json();
+        } catch (error) {
+            console.log(error);
             throw new Error("Failed to create user");
         }
-    };
+    }
+        
 
     const { 
         mutateAsync : createUser, 
